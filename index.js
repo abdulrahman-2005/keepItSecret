8dc599e5099141a8748f5df46b415982cc6efa45
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2023-09-12 16:18:48 +0200

    added delete util

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const contentMainElement = document.getElementById("content");
 
 const secretTemplate = {
-	id: "",
-	title: "",
-	author: "",
-    content: "",
-	password: "",
-    locked: ""
+    id: "asdasfg",
+    title: "Title",
+    author: "Author",
+    content: "CONTENT",
+    password: "124215",
+    locked: true
 };
 
 function viewSecret(secId) {
@@ -14,7 +14,7 @@ function viewSecret(secId) {
         if (secId === secret.id) {
             setTempLSData(secId)
         }
-        window.location.href = "./secret/view"
+        window.location.href = `./secret/view${secId}`
     }
 }
 function crackSecret(secId) {
@@ -22,19 +22,31 @@ function crackSecret(secId) {
         if (secId === secret.id) {
             setTempLSData(secId)
         }
-        window.location.href = "./secret/crack"
+        window.location.href = `./secret/crack#${secId}`
     }
-
 }
 
+function deleteSecret(secId) {
+    for (secret of secretsData.secrets) {
+        if (secId === secret.id) {
+            setTempLSData(secId)
+        }
+        window.location.href = `./secret/delete#${secId}`
+    }
+}
+
+
 function SecretHTML(secretObject) {
-	if (secretObject.locked) {
-		return `
+    if (secretObject.locked) {
+        return `
 <article class="secret" id="${secretObject.id}">
     <div class="left">
         <h4 class="secret-title">${secretObject.title}</h3>
         <h5 class="secret-author">By: <span class="secret-author-span">${secretObject.author}</span></h5>
-        <a  onclick="crackSecret('${secretObject.id}')" class="secret-button">crack</a>
+        <div class="secret-buttons-container">
+            <a onclick="crackSecret('${secretObject.id}')" class="secret-button">crack</a>
+            <a onclick="deleteSecret('${secretObject.id}')" class="secret-button">delete</a>
+        </div>
     </div>
     <div class="right">
         <!-- <img class="secret-state-image" src="./assets/images/locked.png" alt=""> -->
@@ -42,27 +54,30 @@ function SecretHTML(secretObject) {
         <p>locked</p>
     </div>
 </article>`;
-	} else {
-		return `
+    } else {
+        return `
 <article class="secret cracked" id="${secretObject.id}">
     <div class="left">
         <h4 class="secret-title">${secretObject.title}</h3>
         <h5 class="secret-author">By: <span class="secret-author-span">${secretObject.author}</span></h5>
-        <a  onclick="viewSecret('${secretObject.id}')" class="secret-button">view</a>
+        <div class="secret-buttons-container">
+            <a  onclick="viewSecret('${secretObject.id}')" class="secret-button">view</a>
+            <a onclick="deleteSecret('${secretObject.id}')" class="secret-button">delete</a>
+        </div>
     </div>
     <div class="right">
         <p class="icon">🔓</p>
         <p>unlocked</p>
     </div>
 </article>`;
-	}
+    }
 }
 
 function loadSecrets(secretsToLoad) {
-	contentMainElement.innerHTML = "";
-	for (secret of secretsToLoad) {
-		contentMainElement.innerHTML += SecretHTML(secret);
-	}
+    contentMainElement.innerHTML = "";
+    for (secret of secretsToLoad) {
+        contentMainElement.innerHTML += SecretHTML(secret);
+    }
 }
 
 loadSecrets(secretsData.secrets);
@@ -72,6 +87,7 @@ function search(element) {
     let output = []
     let value = element.value.toLowerCase()
     for (secret of secretsData.secrets) {
+
         if (secret.title.toLowerCase().includes(value) || secret.author.includes(value)) {
             output.push(secret)
         }
